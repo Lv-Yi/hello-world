960feabd0a88f3386f386019403a2fa07f005eff
qcarduinoyun <championol@sina.cn>
2015-12-01 20:33:43 +0800

    try again to make sure weixin interface work

--- index.php
+++ index.php
@@ -1,17 +1,11 @@
 <?php
 /*
     方倍工作室 http://www.cnblogs.com/txw1958/
-    CopyRight 2013 www.doucube.com  All Rights Reserved
+    CopyRight 2013  www.doucube.com  All Rights Reserved
 */
-define("TOKEN", "arduinoyun");
+define("TOKEN", "pondbay");
 $wechatObj = new wechatCallbackapiTest();
 $wechatObj->valid();
-if (isset($_GET['echostr'])) {
-    echo $_GET['echostr'];
-    $wechatObj->valid();
-}else{
-    $wechatObj->responseMsg();
-}
 class wechatCallbackapiTest
 {
     public function valid()
@@ -36,35 +30,6 @@ class wechatCallbackapiTest
             return true;
         }else{
             return false;
-        }
-    }
-    public function responseMsg()
-    {
-        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-        if (!empty($postStr)){
-            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-            $fromUsername = $postObj->FromUserName;
-            $toUsername = $postObj->ToUserName;
-            $keyword = trim($postObj->Content);
-            $time = time();
-            $textTpl = "<xml>
-                        <ToUserName><![CDATA[%s]]></ToUserName>
-                        <FromUserName><![CDATA[%s]]></FromUserName>
-                        <CreateTime>%s</CreateTime>
-                        <MsgType><![CDATA[%s]]></MsgType>
-                        <Content><![CDATA[%s]]></Content>
-                        <FuncFlag>0</FuncFlag>
-                        </xml>";
-            if($keyword == "?" || $keyword == "？")
-            {
-                $msgType = "text";
-                $contentStr = date("Y-m-d H:i:s",time());
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                echo $resultStr;
-            }
-        }else{
-            echo "";
-            exit;
         }
     }
 }
