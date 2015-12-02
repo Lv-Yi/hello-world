b8350b43639f53595480dff43f99d5f29ff9434a
qcarduinoyun <championol@sina.cn>
2015-12-02 16:31:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -31,7 +31,9 @@ class wechatCallbackapiTest
             {
                 $msgType = "text";
                 $contentStr1 = date("Y-m-d H:i:s",time());// . pg_conn_string();
-                $contentStr = $contentStr1;
+                $contentStr2 = pg_conn_string();
+                $contentStr = $contentStr1 . $contentStr2;
+                
                 $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
                 echo $resultStr;
             }
