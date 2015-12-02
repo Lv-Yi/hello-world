9511341d3ea1b31e8694b54bc0adb86f3d9fe8f2
qcarduinoyun <championol@sina.cn>
2015-12-02 18:03:14 +0800

    Update index.php

--- index.php
+++ index.php
@@ -50,8 +50,10 @@ class wechatCallbackapiTest
             {
                 $msgType = "text";
                 $contentStr1 = date("Y-m-d H:i:s",time());// . pg_conn_string();
-                $contentStr2 = " local pg_conn_string()";
-                $contentStr = $contentStr1 . $contentStr2 . $contentStr2 . $contentStr2 . $contentStr2 . $contentStr2 . $contentStr2;
+                extract(parse_url($_ENV["DATABASE_URL"]));
+                $contentStr2 = "user=$user password=$pass host=$host dbname=" . substr($path, 1);
+                //$contentStr2 = " local pg_conn_string()";
+                $contentStr = $contentStr1 . $contentStr2;
                 
                 $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
                 echo $resultStr;
