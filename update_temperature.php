a8c52c692c162c9f8a38125172634372b55a7a9d
yilv <yilv@ylxy2a.eicp.net>
2015-12-19 20:25:22 +0800

    test-wx:update temperature, 5th.

--- update_temperature.php
+++ update_temperature.php
@@ -72,7 +72,8 @@
 if ($_GET['data'] && $_GET['id'] && ($_GET['token'] == "arduinoyun")) {//可以改token,这相当于密码，在Arduino端改成相应的值即可
 	$data = $_GET['data'];
 	$id = $_GET['id'];
-	echo pg_set_temperature($id, $data);
+	$retMsg = pg_set_temperature($id, $data);
+    echo $retMsg;
 }else{
 	echo "Permission Denied";//请求中没有type或data或token或token错误时，显示Permission Denied
 }
