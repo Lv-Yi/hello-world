4d10e46ef658a791386e3acf9669c949cb708310
yilv <yilv@ylxy2a.eicp.net>
2015-12-26 16:24:38 +0800

    update wx a.t., 21th

--- weixin.class.php
+++ weixin.class.php
@@ -209,7 +209,8 @@ class wechatCallbackapiTest
             //$pic_data = array("media" => "@".$pic_url);
             $pic_data = self::curl_get_http($pic_url);
             //print_r($pic_data);
-            return $pic_data;
+            //return $pic_data;
+            return count($pic_data, COUNT_RECURSIVE);
             $access_token = self::pg_get_wx_access_token();
             $url = self::wx_url_upload_temp_pic . "access_token=" . $access_token . "&type=image";   //access_token=ACCESS_TOKEN&type=TYPE';
             //$ret = $url;
