acdc498656364ae43925d3de422332a17f3ea0a8
yilv <yilv@ylxy2a.eicp.net>
2015-12-26 16:53:40 +0800

    update wx a.t., 21th

--- weixin.class.php
+++ weixin.class.php
@@ -206,9 +206,10 @@ class wechatCallbackapiTest
             $pic_data = $img;
             //----------------------------------------------------
             */
-            //$pic_data = array("media" => "@".$pic_url);
             //$pic_data = self::curl_get_http($pic_url);
             $pic_data = file_get_contents($pic_url);
+            //$pic_data = array("media" => "@".$pic_url);
+            $pic_data = array("media" => $pic_data);
             //print_r($pic_data);
             //return $pic_data;
             //return strlen($pic_data);
