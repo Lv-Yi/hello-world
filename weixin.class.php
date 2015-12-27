074341f90c4811ef8fb1e076e4e5a4a30c7007f3
yilv <yilv@ylxy2a.eicp.net>
2015-12-27 14:21:03 +0800

    update wx a.t., 21th

--- weixin.class.php
+++ weixin.class.php
@@ -285,16 +285,16 @@ class wechatCallbackapiTest
                 }*/
                 // try to upload pic
                 //$contentStr .= ", upload pic url: " . "http://".$arr_config['host_ext_ip'].":8112/shot.jpg";
-                $contentStr .= self::curl_upload_wx_pic("http://".$arr_config['host_ext_ip'].":8112/shot.jpg");
-                //$contentStr .= self::curl_upload_wx_pic("http://ooopic.assetsdelivery.com/168nwm/carodi/carodi1011/carodi101100034.jpg");
+                //$contentStr .= self::curl_upload_wx_pic("http://".$arr_config['host_ext_ip'].":8112/shot.jpg");
+                $contentStr .= self::curl_upload_wx_pic("http://ooopic.assetsdelivery.com/168nwm/carodi/carodi1011/carodi101100034.jpg");
 
             } else if ($keyword == "~")
             {
                 $msgType = "image"; $textTpl = $picRpl;
                 //$msgType = "text";
-                $arr_config = self::pg_get_wx_config_all();
-                //$media_id1 = self::curl_upload_wx_pic("http://ooopic.assetsdelivery.com/168nwm/carodi/carodi1011/carodi101100034.jpg");
-                $media_id1 = self::curl_upload_wx_pic("http://".$arr_config['host_ext_ip'].":8112/shot.jpg");
+                $media_id1 = self::curl_upload_wx_pic("http://ooopic.assetsdelivery.com/168nwm/carodi/carodi1011/carodi101100034.jpg");
+                //$arr_config = self::pg_get_wx_config_all();
+                //$media_id1 = self::curl_upload_wx_pic("http://".$arr_config['host_ext_ip'].":8112/shot.jpg");
                 //$media_id1 = "ODm6iMxxqoKUyurCpDWB_2zVY3lR4JW4EXbuidaBX6I1MndKyfT2zPXTa1vUcC6Y";
                 $contentStr .= "media id: " . $media_id1;
                 //$contentStr .= $media_id1;
