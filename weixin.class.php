5135722736835e28ade1383253dc074a7d840bbe
yilv <yilv@ylxy2a.eicp.net>
2015-12-27 14:13:19 +0800

    update wx a.t., 21th

--- weixin.class.php
+++ weixin.class.php
@@ -293,7 +293,7 @@ class wechatCallbackapiTest
                 //$msgType = "text";
                 //$arr_config = self::pg_get_wx_config_all();
                 //$media_id1 = self::curl_upload_wx_pic("http://ooopic.assetsdelivery.com/168nwm/carodi/carodi1011/carodi101100034.jpg");
-                $media_id1 = self::curl_upload_wx_pic(self::pg_get_wx_access_token(), "http://".$arr_config['host_ext_ip'].":8112/shot.jpg");
+                $media_id1 = self::curl_upload_wx_pic("http://".$arr_config['host_ext_ip'].":8112/shot.jpg");
                 //$media_id1 = "ODm6iMxxqoKUyurCpDWB_2zVY3lR4JW4EXbuidaBX6I1MndKyfT2zPXTa1vUcC6Y";
                 $contentStr .= "media id: " . $media_id1;
                 //$contentStr .= $media_id1;
